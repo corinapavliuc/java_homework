15900c4033ca8f8cb52ab33c01eac404ab0d155a
corinapavliuc <138014725+corinapavliuc@users.noreply.github.com>
2023-07-11 12:21:09 +0200

    Create Main.java
    homework

---
+++ Main.java
@@ -0,0 +1,66 @@
+package homework;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        char ch = 'G';
+
+        int num = 89;
+
+        byte num_1 = 4;
+
+        short num_2 = 56;
+
+        float num_3 = 4.7333436F;
+
+        double num_4 = 4.355453532;
+
+        long num_5 = 12121;
+
+
+
+        System.out.println("Character: " + ch);
+
+        System.out.println("Integer: " + num);
+
+        System.out.println("Byte: " + num_1);
+
+        System.out.println("Short: " + num_2);
+
+        System.out.println("Float: " + num_3);
+
+        System.out.println("Double: " + num_4);
+
+        System.out.println("Long: " + num_5);
+
+
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println("____________________________________________________________");
+
+
+
+
+
+        int number = 345;
+
+        int firstNum = number / 100;
+
+        int secNum = number % 100 / 10;
+
+        int trNum = number % 10;
+
+        System.out.println("Число " + number + " - > " + firstNum + "," + secNum + "," + trNum + ".");
+
+
+    }
